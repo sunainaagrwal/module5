c28a369838298bd3cbda99ed61bb205c4f53e5f7
sunainaagrwal <67600469+sunainaagrwal@users.noreply.github.com>
2020-06-29 22:43:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speakBye(names[i]);
+      } else {
+        helloSpeaker.speakHello(names[i]);
+      }
+    }
+    
+    })();
