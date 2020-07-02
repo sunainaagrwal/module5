13310fdddf8b371fc5a5dc167b66ea2df6203108
sunainaagrwal <67600469+sunainaagrwal@users.noreply.github.com>
2020-07-02 22:16:53 +0530

    Delete saygoodbye.js

--- saygoodbye.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-  var byeSpeaker={};
-  var speakWord = "Good Bye"; 
-  byeSpeaker.speakBye=function speak(name) {
-    console.log(speakWord + " " + name);
-  }   
-
-  window.byeSpeaker=byeSpeaker;
-})(window);
