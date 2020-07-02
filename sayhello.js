5815961b383e5d0c12b4a9a89d23012b73b09cdd
sunainaagrwal <67600469+sunainaagrwal@users.noreply.github.com>
2020-07-02 22:17:10 +0530

    Delete sayhello.js

--- sayhello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-  var helloSpeaker={};
-  var speakWord = "Hello"; 
-  helloSpeaker.speakHello=function speak(name) {
-    console.log(speakWord + " " + name);
-  }   
-
-  window.helloSpeaker=helloSpeaker;
-})(window);
